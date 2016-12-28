23e2303f9cb7a4de5126240e84556ac5919397b7
gazza6 <lshgary@hotmail.com>
2016-12-28 17:59:52 +1300

    Skeleton get data from three table done in test

--- src/playSQL/Connect.java
+++ src/playSQL/Connect.java
@@ -5,9 +5,15 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+import com.sun.rowset.CachedRowSetImpl;
+
 import application.Bidder;
 import application.BidderDAO;
 import application.DBUtil;
+import application.Deck;
+import application.DeckDAO;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 public class Connect {
 	public static void main (String[] args) {
@@ -16,22 +22,28 @@ public class Connect {
 			Connection conn=DriverManager.getConnection(  
 					"jdbc:mysql://localhost:3306/Cards","root","root");  
 			System.out.println("connected");
+			
 			Statement stmt = conn.createStatement();
 			//			String updateStmt =
 			//					"SELECT * FROM Bidder WHERE Name='aa'";
 			//			DBUtil.dbExecuteQuery(updateStmt);
 			//			Bidder bidder = BidderDAO.searchBidder("1");
 			//			System.out.println("Added");
-
-			ResultSet rs = stmt.executeQuery("SELECT * FROM Bidder");
-			Bidder bidder = new Bidder();
-			if(rs.next()) { 
-				int id = rs.getInt("ID"); 
-				String str1 = rs.getString("Name");
-				bidder.setId(id); 
-				bidder.setName(str1); 
-				System.out.println(id);
-				System.out.println(str1);
+			 CachedRowSetImpl crs = null;
+			ResultSet rs = stmt.executeQuery("SELECT d.Name, d.DeckCondition, o.Price, o.BidderID, b.Name, o.Date FROM Deck d LEFT JOIN Offer o on d.ID = o.DeckID left join Bidder b on o.BidderID = b.ID;");
+			while(rs.next()) { 
+//				int id = rs.getInt("ID"); 
+//				String str1 = rs.getString("Name");
+//				bidder.setId(id); 
+//				bidder.setName(str1); 
+//				System.out.println(id);
+//				System.out.println(str1);
+				System.out.println("Deck name: "+rs.getString("d.Name"));
+				System.out.println("Deck condition: "+rs.getString("d.DeckCondition"));
+				System.out.println("Deck price: "+rs.getString("o.Price"));
+				System.out.println("Deck bidder name: "+rs.getString("b.Name"));
+				System.out.println("Deck date: "+rs.getString("o.Date"));
+				System.out.println("==================");
 			}
 
 			conn.close();
